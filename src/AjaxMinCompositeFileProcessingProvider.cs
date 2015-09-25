a47488ad9ee384566e5656020a531b3ff0cec1eb
Brian Dukes <bdukes@engagesoftware.com>
2015-09-25 15:15:24 -0500

    Add AjaxMinCompositeFileProcessingProvider

---
+++ src/AjaxMinCompositeFileProcessingProvider.cs
@@ -0,0 +1,73 @@
+﻿// <copyright file="AjaxMinCompositeFileProcessingProvider.cs" company="Engage Software">
+// Copyright (c) 2015
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+namespace Engage.Dnn
+{
+    using System.Linq;
+
+    using ClientDependency.Core;
+    using ClientDependency.Core.CompositeFiles.Providers;
+
+    using DotNetNuke.Web.Client;
+
+    using JetBrains.Annotations;
+
+    using Microsoft.Ajax.Utilities;
+
+    /// <summary>A provider for combining, minifying, compressing and saving composite scripts/css files</summary>
+    [PublicAPI]
+    public class AjaxMinCompositeFileProcessingProvider : CompositeFileProcessingProvider
+    {
+        /// <summary>Settings for minifying CSS</summary>
+        private static readonly CssSettings CssSettings = new CssSettings { TermSemicolons = true, CommentMode = CssComment.Hacks, };
+
+        /// <summary>Settings for minifying JavaScript code</summary>
+        private static readonly CodeSettings JsSettings = new CodeSettings { TermSemicolons = true, EvalTreatment = EvalTreatment.MakeAllSafe, InlineSafeStrings = false, };
+
+        /// <summary>The minifier</summary>
+        private readonly Minifier minifier = new Minifier();
+
+        /// <summary>The client resource settings</summary>
+        private readonly ClientResourceSettings clientResourceSettings = new ClientResourceSettings();
+
+        /// <summary>Gets a value indicating whether CSS is configured to be minified.</summary>
+        private bool MinifyCss => this.clientResourceSettings.EnableCssMinification() ?? this.EnableCssMinify;
+
+        /// <summary>Gets a value indicating whether JavaScript is configured to be minified.</summary>
+        private bool MinifyJs => this.clientResourceSettings.EnableJsMinification() ?? this.EnableJsMinify;
+
+        /// <summary>Minifies the given <paramref name="fileContents"/>.</summary>
+        /// <param name="fileContents">The contents of the file.</param>
+        /// <param name="type">The type of the resource.</param>
+        /// <returns>The minified or full contents of <paramref name="fileContents"/>, based on the minification configuration.</returns>
+        protected override string MinifyFile(string fileContents, ClientDependencyType type)
+        {
+            if (type == ClientDependencyType.Css && this.MinifyCss)
+            {
+                var minifiedCss = this.minifier.MinifyStyleSheet(fileContents, CssSettings);
+                if (!this.minifier.ErrorList.Any(error => error.IsError))
+                {
+                    return minifiedCss;
+                }
+            }
+
+            if (type == ClientDependencyType.Javascript && this.MinifyJs)
+            {
+                var minifiedScript = this.minifier.MinifyJavaScript(fileContents, JsSettings);
+                if (!this.minifier.ErrorList.Any(error => error.IsError))
+                {
+                    return minifiedScript;
+                }
+            }
+
+            return fileContents;
+        }
+    }
+}
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Engage Software">
+// Copyright (c) 2015
+// by Engage Software ( http://www.engagesoftware.com )
+// </copyright>
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Engage AjaxMinCompositeFileProcessingProvider")]
+[assembly: AssemblyDescription("A CompositeFileProcessingProvider for DNN's CDF that uses AjaxMin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Engage Software")]
+[assembly: AssemblyProduct("Engage.Dnn.Properties")]
+[assembly: AssemblyCopyright("2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(false)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aa91c6ac-f4cf-4203-baad-2eddff0ad26b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
