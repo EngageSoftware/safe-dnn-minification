435bd55c9e6d2c269334224d40e552aa9e98194a
Brian Dukes <bdukes@engagesoftware.com>
2015-09-25 17:08:18 -0500

    Add build process
    Build and zip via FAKE

---
+++ build/build.fsx
@@ -0,0 +1,38 @@
+// include Fake lib
+#r @"..\packages\FAKE\tools\FakeLib.dll"
+open Fake
+
+RestorePackages()
+
+let tempDir = "./tmp/"
+let binDir = "./tmp/bin"
+let distDir = "../dist/"
+let rootDir = "../"
+let srcDir = "../src/"
+
+Target "Clean" (fun _ ->
+    CleanDirs [tempDir; distDir]
+)
+
+Target "Build" (fun _ ->
+    !! (srcDir + "*.csproj")
+      |> MSBuildRelease binDir "Build"
+      |> Log "Build Output: "
+)
+
+Target "Package" (fun _ ->
+    !! (tempDir + "**/*")
+      ++ (srcDir + "*.dnn")
+      ++ (rootDir + "LICENSE.htm")
+      ++ (rootDir + "CHANGES.htm")
+      |> Zip tempDir (distDir + "Engage-SafeDnnMinification_Install.zip")
+)
+
+// Dependencies
+"Clean"
+  ==> "Build"
+  ==> "Package"
+
+
+// start build
+RunTargetOrDefault "Package"
